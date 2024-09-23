3a288f90e936999d83a3be3ad9d7e3974f84f200
Paul OHL <paul.lv.ohl@gmail.com>
2024-09-23 12:03:26 +0200

    feat: Day 02 part 2

--- src/days/day_02.rs
+++ src/days/day_02.rs
@@ -34,3 +34,40 @@ pub fn solve_part_1() -> i32 {
         }
     })
 }
+
+pub fn solve_part_2() -> i32 {
+    let input = crate::utils::read_input_file("day_02.txt");
+
+    input.lines().fold(0, |sum, current_line| {
+        let result: (i32, i32, i32) = current_line
+            .split_once(':')
+            .unwrap()
+            .1
+            .trim()
+            .split(';')
+            .fold((0, 0, 0), |rgb_values, hand_content| {
+                let mut green = 0;
+                let mut red = 0;
+                let mut blue = 0;
+
+                for color in hand_content.trim().split(',') {
+                    let (num, color) = color.trim().split_once(' ').unwrap();
+                    let num: i32 = num.parse().expect("Wrong format!");
+                    match color {
+                        "red" => red = num,
+                        "green" => green = num,
+                        "blue" => blue = num,
+                        _ => panic!("Color didn't match red, green or blue"),
+                    }
+                }
+
+                (
+                    rgb_values.0.max(red),
+                    rgb_values.1.max(green),
+                    rgb_values.2.max(blue),
+                )
+            });
+
+        sum + result.0 * result.1 * result.2
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     // println!("day01 (part 01): {:?}", days::day_01::solve_part_1());
     // println!("day01 (part 02): {:?}", days::day_01::solve_part_2());
 
-    println!("day02 (part 01): {:?}", days::day_02::solve_part_1());
-    // println!("day02 (part 02): {:?}", days::day_02::solve_part_2());
+    // println!("day02 (part 01): {:?}", days::day_02::solve_part_1());
+    println!("day02 (part 02): {:?}", days::day_02::solve_part_2());
 }
